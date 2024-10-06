a70332012d52dedd9eb3f0a69c99c3008f47294b
Dani Fadli <danifadliirmawn@gmail.com>
2024-10-06 14:12:47 +0700

    Initialize MongoDB Connection

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,6 +30,16 @@ return [
     */
 
     'connections' => [
+
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options' => [],
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -147,7 +157,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+Route::get('/info', function () {
+    return phpinfo();
+});
+
+Route::get('/ping', function () {
+    $connection = DB::connection('mongodb');
+    $msg = 'MongoDB is accessible!';;
+    try {
+        $connection->command(['ping' => 1]);
+    } catch (\Exception  $e) {
+        $msg = 'MongoDB is not accessible. Error: ' . $e->getMessage();
+    }
+    return ['msg' => $msg];
+});
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
